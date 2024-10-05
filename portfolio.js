f604bb19c7a5fc67dce9449cb8fd1be5ceddbced
Raka Febrian Putra P <pramuditaraka448@gmail.com>
2024-10-06 01:08:37 +0700

    Menambahkan fitur portfolio

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+const portfolio = [
+    {
+        title: "Proyek 1",
+        description: "Deskripsi proyek 1",
+        link: "http://linkkeproyek1.com"
+    },
+    {
+        title: "Proyek 2",
+        description: "Deskripsi proyek 2",
+        link: "http://linkkeproyek2.com"
+    }
+];
+
+module.exports = portfolio;
